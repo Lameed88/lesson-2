bf6d8bd87ec40f006c3b8f61574c5cefb2d0cc7a
Lameed88 <moradeyor88@gmail.com>
2023-11-04 22:34:05 +0100

    update

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import './index.css'
+import Header from './Header'
+import Content from './Content'
+import Footer from './Footer'
+import ListComponet from './ListComponet'
+
+
+function App() {
+
+
+  return (
+    <div className='App'>
+
+
+    
+        {/* Hello  {handleNameChange()} */}
+        <Header />
+        <Content />
+        <Footer /> 
+     
+     
+
+                                     { /*Custom Element*/}
+    
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+    
+    
+    return (
+        <header>
+            <h1> WELCOME TO REACT-JS CLASS </h1>
+         
+        </header>
+    )
+}
+
+export default Header
